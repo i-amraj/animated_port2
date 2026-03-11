4e9175e112891be63df12633b148fa004d233d70
i-amraj <iamrajsoni1@gmail.com>
2026-03-11 16:30:51 +0530

    chore: remove gsap-trial plugins to fix production redirect issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,26 +2,29 @@ import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import Lenis from "lenis";
 import "./styles/Navbar.css";
 
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
-export let smoother: ScrollSmoother;
+gsap.registerPlugin(ScrollTrigger);
+export let lenis: Lenis;
 
 const Navbar = () => {
   useEffect(() => {
-    smoother = ScrollSmoother.create({
-      wrapper: "#smooth-wrapper",
-      content: "#smooth-content",
-      smooth: 1.7,
-      speed: 1.7,
-      effects: true,
+    lenis = new Lenis({
       autoResize: true,
-      ignoreMobileResize: true,
+      duration: 1.7,
     });
 
-    smoother.scrollTop(0);
-    smoother.paused(true);
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    lenis.scrollTo(0, { immediate: true });
+    lenis.stop();
 
     let links = document.querySelectorAll(".header ul a");
     links.forEach((elem) => {
@@ -29,15 +32,18 @@ const Navbar = () => {
       element.addEventListener("click", (e) => {
         if (window.innerWidth > 1024) {
           e.preventDefault();
-          let elem = e.currentTarget as HTMLAnchorElement;
-          let section = elem.getAttribute("data-href");
-          smoother.scrollTo(section, true, "top top");
+          let currentElem = e.currentTarget as HTMLAnchorElement;
+          let section = currentElem.getAttribute("data-href");
+          if (section) {
+            lenis.scrollTo(section);
+          }
         }
       });
     });
-    window.addEventListener("resize", () => {
-      ScrollSmoother.refresh(true);
-    });
+
+    return () => {
+      lenis.destroy();
+    };
   }, []);
   return (
     <>
