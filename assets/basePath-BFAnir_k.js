536bbd029dc63ef27e8a5dd539fc6fd27f7ffedf
i-amraj <iamrajsoni1@gmail.com>
2026-03-17 18:09:33 +0530

    Updates

---
+++ assets/basePath-BFAnir_k.js
@@ -0,0 +1,1 @@
+const a=s=>{const t="/",e=s.startsWith("/")?s.slice(1):s;return`${t}${e}`};export{a as g};
--- assets/basePath-C_SfcmUN.js
+++
@@ -1,1 +0,0 @@
-const a=t=>{const s="/animated_port2/",e=t.startsWith("/")?t.slice(1):t;return`${s}${e}`};export{a as g};
